c146202121467b90b173b949f5c2551d79d0bd4f
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-21 21:11:02 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Omolemo Mashigo on 2025/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Omolemo Mashigo on 2025/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap Me"){
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
