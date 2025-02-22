15e668cecdd62b74e30deeed160c141b2832849d
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-22 20:48:32 +0200

    using delayed animations with gestures

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,26 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
     @State private var amountDragged = CGSize.zero
     
-    @State private var animationAmount = 0.0
     
     var body: some View {
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(amountDragged)
-            .gesture(
-                DragGesture()
-                    .onChanged{amountDragged = $0.translation}
-                    .onEnded{_ in
-                        withAnimation(.bouncy){ //explicit animation approach
-                            amountDragged = .zero
-                        }
-                    }
-            )
-            .animation(.bouncy, value: amountDragged) //implicit animation approach
         
+        HStack(spacing: 0){
+            ForEach(0..<letters.count, id: \.self){ num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(amountDragged)
+                    .animation(.linear.delay(Double(num)/20), value: amountDragged)
+                   
+            }
+        }//attach the gesture to whe whole HStack
+        .gesture(
+            DragGesture()
+                .onChanged{amountDragged = $0.translation}
+                .onEnded{ _ in
+                    amountDragged = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
